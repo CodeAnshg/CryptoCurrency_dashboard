41469fa01a2b1838148d36807f83fe92cf074531
Ansh Gupta <guptaansh912@gmail.com>
2024-07-17 19:45:55 +0530

    Add untracked files before merging

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Coins from './Components/Coins/Coins.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Coins/>
+
+
+      
+
+    </>
+  )
+}
+
+export default App
